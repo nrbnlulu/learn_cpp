76d64370c47ff77b524e085ea52696d73f2f5ffc
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-01-09 20:48:38 +0200

    https://github.com/ttroy50/cmake-examples/tree/master/01-basic/B-hello-headers

---
+++ src/SampleLib.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "SampleLib.h"
+
+void Sample::sayHi(const char *name){
+    std::cout << "Hello " << name << std::endl;
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "SampleLib.h"
+
+
+int main(int argc, char **argv){
+    Sample::sayHi("Nir");
+    return 0;
+}
